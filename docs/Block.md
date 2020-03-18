abb00e157745e25cd8e58e3dbe47962d2d6bac64
KSF Media CI <web+anka@ksfmedia.fi>
2020-03-18 14:42:12 +0000

    Update client for lettera@a8c9dc21

--- docs/Block.md
+++ docs/Block.md
@@ -8,6 +8,8 @@ Name | Type | Description | Notes
 **image** | [**ImageInfo**](ImageInfo.md) |  | [optional] 
 **factBox** | [**FactInfo**](FactInfo.md) |  | [optional] 
 **headline** | **String** |  | [optional] 
+**footnote** | **String** |  | [optional] 
+**question** | **String** |  | [optional] 
 **quote** | **String** |  | [optional] 
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
--- lettera/Classes/OpenAPIs/Models/Block.swift
+++ lettera/Classes/OpenAPIs/Models/Block.swift
@@ -16,14 +16,18 @@ public struct Block: Codable {
     public var image: ImageInfo?
     public var factBox: FactInfo?
     public var headline: String?
+    public var footnote: String?
+    public var question: String?
     public var quote: String?
 
-    public init(paragraph: String?, html: String?, image: ImageInfo?, factBox: FactInfo?, headline: String?, quote: String?) {
+    public init(paragraph: String?, html: String?, image: ImageInfo?, factBox: FactInfo?, headline: String?, footnote: String?, question: String?, quote: String?) {
         self.paragraph = paragraph
         self.html = html
         self.image = image
         self.factBox = factBox
         self.headline = headline
+        self.footnote = footnote
+        self.question = question
         self.quote = quote
     }
 
